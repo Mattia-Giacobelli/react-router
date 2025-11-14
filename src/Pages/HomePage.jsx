f479fc5fd49e4e516c0af362524aec1958150afe
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-11-14 13:19:00 +0100

    Create HomePage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,25 @@
+import { NavLink } from "react-router-dom";
+
 export default function HomePage() {
 
 
     return (
 
         <>
+            <div className="p-5 mb-4 bg-light rounded-3 vh-100">
+                <div className="container-fluid py-5">
+                    <h1 className="display-5 fw-bold">WELCOME TO FAKESTORE</h1>
+                    <p className="col-md-8 fs-4">
+                        We are a new e-commerce website full of random products that you can buy with a
+                        <strong> TWIST</strong>:
+                        <br />
+                        you'll recive a rubberduck to discuss about how you got scammed on our website
+                    </p>
+                    <NavLink className="btn btn-primary btn-lg" type="button" to="/Products" >
+                        Products
+                    </NavLink>
+                </div>
+            </div>
 
         </>
 
