4c76a64091c5759249903d6000387f8fed4c2ad7
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-11-14 13:36:22 +0100

    Create axios call

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,13 +6,13 @@ export default function Header() {
     return (
 
         <header>
-            <nav class="navbar navbar-expand-sm navbar-light bg-primary">
-                <div class="container-fluid">
-                    <NavLink class="navbar-brand " id="always-active" to="/">FAKE STORE</NavLink>
-                    <div class="navbar-nav">
-                        <NavLink class="nav-link" aria-current="page" to="/" >Home</NavLink>
-                        <NavLink class="nav-link" aria-current="page" to="/Products">Products</NavLink>
-                        <NavLink class="nav-link" aria-current="page" to="/AboutUs">AboutUs</NavLink>
+            <nav className="navbar navbar-expand-sm navbar-light bg-primary">
+                <div className="container-fluid">
+                    <NavLink className="navbar-brand " id="always-active" to="/">FAKE STORE</NavLink>
+                    <div className="navbar-nav">
+                        <NavLink className="nav-link" aria-current="page" to="/" >Home</NavLink>
+                        <NavLink className="nav-link" aria-current="page" to="/Products">Products</NavLink>
+                        <NavLink className="nav-link" aria-current="page" to="/AboutUs">AboutUs</NavLink>
                     </div>
 
                 </div>
--- src/Pages/ProductsPage.jsx
+++ src/Pages/ProductsPage.jsx
@@ -1,5 +1,28 @@
+import axios from "axios"
+import { useState } from "react"
+
+
 export default function ProductsPage() {
 
+    //Create Axios call
+    const productsApi = 'https://fakestoreapi.com/products'
 
-    return
+    function getProducts() {
+
+        axios.get(productsApi)
+            .then(res => console.log(res.data)
+            )
+    }
+
+    useState(getProducts, [])
+
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-4"></div>
+                </div>
+            </div>
+        </>
+    )
 }
