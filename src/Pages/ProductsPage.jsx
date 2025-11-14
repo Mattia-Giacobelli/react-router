b6fa4c93e66aa7c8a096b5a05ef4a77b36e72142
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-11-14 14:19:46 +0100

    Create Products Page

--- src/Pages/ProductsPage.jsx
+++ src/Pages/ProductsPage.jsx
@@ -4,13 +4,17 @@ import { useState } from "react"
 
 export default function ProductsPage() {
 
+    //Create variable to store products data
+    const [products, setProducts] = useState([])
+
+
     //Create Axios call
     const productsApi = 'https://fakestoreapi.com/products'
 
     function getProducts() {
 
         axios.get(productsApi)
-            .then(res => console.log(res.data)
+            .then(res => setProducts(res.data)
             )
     }
 
@@ -19,8 +23,38 @@ export default function ProductsPage() {
     return (
         <>
             <div className="container">
-                <div className="row">
-                    <div className="col-4"></div>
+                <div className="row mt-4">
+                    {products.map(product => {
+
+                        return (
+                            <div key={product.id} className="col-3 mb-3">
+                                <div className="card h-100">
+                                    <div className="img-cont">
+                                        <img src={product.image} className="card-img-top w-100" alt={product.image} />
+                                    </div>
+
+                                    <div className="card-body">
+                                        <h6 className="card-title">{product.title}</h6>
+                                        <p className="card-text">{product.price}€</p>
+
+
+                                    </div>
+
+                                    <div className="rating">
+                                        <span className="card-text pe-1">
+                                            {product.rating.rate}/5
+                                        </span>
+                                        <span className="text-warning">
+                                            <i class="bi bi-star-fill"></i>
+                                        </span>
+                                        <span className="ps-2">
+                                            {product.rating.count}
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </>
